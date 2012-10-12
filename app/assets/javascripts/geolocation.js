42994d3c3e651d4a650ee6dd5ea6eb2b49a27763
Stig Palmquist <stigtsp@gmail.com>
2012-10-12 21:37:48 +0800

    - First dummy of item loader

---
+++ app/assets/javascripts/geolocation.js
@@ -0,0 +1,11 @@
+var watchPositionId;
+$('[data-role="page"]').live('pageshow', function(e) {
+  if (!watchPositionId) {
+	 watchPositionId = navigator.geolocation.watchPosition(function(p){$(window).trigger('positionUpdate', p);});
+  }
+});
+
+var currentGeoLocation;
+$(window).on('positionUpdate', function(e, pos) {
+  currentGeoLocation = pos;
+});
